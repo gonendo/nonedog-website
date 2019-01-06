c4c485f9958bf71f7eb783d37a1bc430f7f3e049
Brice Andriantafika <shikigami@gmail.com>
2019-01-06 23:06:28 +0100

    try to fix stylus

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 var home = require('./routes/home');
 
 var express = require('express');
+var fs = require('fs')
 var stylus = require('stylus');
 var nib = require('nib');
+var path = require('path');
 var app = express();
 
 app.set('view engine', 'pug');
 
-function compile(str, path) {
-  return stylus(str)
-      .set('filename', path)
-      .use(nib())
+// Compile the stylus files into css
+var dir = __dirname + '/public/css';
+try {
+  var list = fs.readdirSync(dir);
+  list.forEach(function(file){
+    if(path.extname(file)=='.styl'){
+      var str = fs.readFileSync(__dirname + '/public/css/' + file, 'utf8');
+      stylus(str)
+        .use(nib())
+        .render(function(err, css){
+          if (err) throw err;
+          fs.writeFile(__dirname + '/public/css/' + path.basename(file, '.styl') + '.css', css, function(err) {
+            if (err) throw err;
+          });
+  });
+    }
+  });
+} catch (error) {
+  console.error(error);
 }
-
-app.use(stylus.middleware({
-  src: __dirname + '/public',
-  compile: compile
-}));
-
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', home.home);
 
