906b470ec59eaa2d5a96e7772a2da7661105b773
Brice Andriantafika <shikigami@gmail.com>
2019-01-06 17:14:55 +0100

    added stylus

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
+var home = require('./routes/home');
+
 var express = require('express');
+var stylus = require('stylus');
+var nib = require('nib');
 var app = express();
 
 app.set('view engine', 'pug');
 
-app.get('/', function(req, res) {
-  console.log(app.get('views'));
-  res.render('index', { title: "Gonendo's website", message: "WIP"});
-});
+function compile(str, path) {
+  return stylus(str)
+      .set('filename', path)
+      .use(nib())
+}
 
-app.post('/', function(req, res) {
-});
+app.use(stylus.middleware({
+  src: __dirname + '/public',
+  compile: compile
+}));
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', home.home);
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app
