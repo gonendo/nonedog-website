e40f3743cf7288dd638c6092ccf5d7f09f235f2a
Gonendo <shikigami@gmail.com>
2022-05-27 03:10:31 +0200

    added simple test

---
+++ tests/supertest.js
@@ -0,0 +1,15 @@
+const supertest = require('supertest');
+const assert = require('assert');
+const app = require("../app")
+
+describe("GET /", () => {
+    it("It should has status code 200", (done) => {
+        supertest(app)
+            .get("/")
+            .expect(200)
+            .end((err,res) => {
+                if (err) done(err);
+                done();
+            })
+    });
+});
